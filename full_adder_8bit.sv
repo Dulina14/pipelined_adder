c4465414c1465383f1aa86b240625d4d535ba44b
Dulina Hansaja Ranaweera <168477910+Dulina14@users.noreply.github.com>
2025-08-18 22:45:46 +0530

    full_adder_8bit changed

--- full_adder_8bit.sv
+++ full_adder_8bit.sv
@@ -1,8 +1,12 @@
-module full_adder_8bit (
-    input  logic [7:0] a, b,
-    input  logic       cin,
-    output logic [7:0] sum,
-    output logic       cout
-);
-    assign {cout, sum} = a + b + cin;
+module full_adder_8bit(
+        input [7:0]num1,num2,
+        input cin,
+        output [7:0]sum,
+        output cout
+        );
+    
+    wire    [8:0] sum_complete;
+    assign  sum_complete = {1'b0, num1} + {1'b0, num2} + {7'b0, cin};
+    assign  sum = sum_complete[7:0];
+    assign  cout = sum_complete[8];
 endmodule
